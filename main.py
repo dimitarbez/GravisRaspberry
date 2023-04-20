684fb18b10f788e660062d4f79bf838f94a11dda
Dimitar Bezhanovski <dimitar.bezhanovski@symphony.is>
2023-04-20 19:19:20 +0200

    lights command

--- main.py
+++ main.py
@@ -37,73 +37,58 @@ side_border_color = (0, 0, 255)
 
 ser = serial.Serial('/dev/ttyUSB0', baudrate=115200, timeout=1)
 
+is_light_on = False
+
 
 def on_press(key):
+
     try:
         print('alphanumeric key {0} pressed'.format(
             key.char))
         if key.char == ('w'):
             print('sent')
             ser.write('motor:forward\n'.encode())
-            data = ser.read(size=3).decode()
-            print(data)
-            print(data.decode())
-            print('sent')
 
         if key.char == ('a'):
             ser.write('motor:left\n'.encode())
-            data = ser.read(size=3).decode()
-            print(data)
-            print(data.decode())
 
         if key.char == ('s'):
             print('sent')
             ser.write('motor:backward\n'.encode())
-            data = ser.read(size=3).decode()
-            print(data)
-            print(data.decode())
 
         if key.char == ('d'):
             print('sent')
             ser.write('motor:right\n'.encode())
-            data = ser.read(size=3).decode()
-            print(data)
-            print(data.decode())
 
         elif key.char == ('1'):
             print('sent')
             ser.write('motor:speed:80\n'.encode())
-            data = ser.read(size=3).decode()
-            print(data)
-            print(data.decode())
 
         elif key.char == ('2'):
             print('sent')
             ser.write('motor:speed:120\n'.encode())
-            data = ser.read(size=3).decode()
-            print(data)
-            print(data.decode())
 
         elif key.char == ('3'):
             print('sent')
             ser.write('motor:speed:150\n'.encode())
-            data = ser.read(size=3).decode()
-            print(data)
-            print(data.decode())
 
         elif key.char == ('p'):
             print('sent')
             ser.write('servo:armheight:150\n'.encode())
-            data = ser.read(size=3).decode()
-            print(data)
-            print(data.decode())
 
         elif key.char == ('o'):
             print('sent')
             ser.write('servo:armheight:90\n'.encode())
-            data = ser.read(size=3).decode()
-            print(data)
-            print(data.decode())
+
+        elif key.char == ('l'):
+            if is_light_on:
+                ser.write('lights:front:0:0:0')
+                ser.write('lights:front:0:0:0')
+            if not is_light_on:
+                ser.write('lights:front:255:255:255')
+                ser.write('lights:front:255:0:0')
+              
+            is_light_on = not is_light_on
 
     except AttributeError:
         print('special key {0} pressed'.format(
@@ -121,7 +106,7 @@ def on_release(key):
     if key == keyboard.Key.esc:
         # Stop listener
         return False
-    
+
 
 def opencv_code():
     # give camera time to warm up
@@ -132,50 +117,6 @@ def opencv_code():
     for still in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
       # take the frame as an array, convert it to black and white, and look for facial features
         image = still.array
-        gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-
-        face = face_tracker.get_face_from_image(gray, faceCascade)
-        if len(face) == 0:
-            continue
-
-        face_x = face[0]
-        face_y = face[1]
-        face_width = face[2]
-        face_height = face[3]
-
-        # formula for object position
-        object_in_right_area = face_tracker.is_face_in_right_region(
-            face, image)
-        object_in_left_area = face_tracker.is_face_in_left_region(face)
-
-        cv.rectangle(image, (face_x, face_y), (face_x+face_width,
-                     face_y+face_height), tracked_face_color, 2)
-
-        # within the left region
-        if object_in_left_area and not object_in_right_area:
-
-            print('left')
-
-        # within the right region
-        elif object_in_right_area and not object_in_left_area:
-            print('right')
-
-        elif (object_in_left_area and object_in_right_area) or not (object_in_left_area and object_in_right_area):
-            print('center')
-            if face_width > max_face_tracking_width:
-                print('move backward')
-
-            elif face_width < min_face_tracking_width:
-                print('move forward')
-
-            else:
-                print('stop')
-
-        # draw side borders
-        cv.line(image, (side_borders_distance, 0),
-                (side_borders_distance, image.shape[0]), side_border_color, 5)
-        cv.line(image, (image.shape[1] - side_borders_distance, 0),
-                (image.shape[1] - side_borders_distance, image.shape[0]), side_border_color, 5)
 
         # display the resulting image
         cv.imshow("Display", image)
@@ -190,7 +131,7 @@ def opencv_code():
 
 
 def robot_drive_code():
-        # Collect events until released
+    # Collect events until released
     with keyboard.Listener(
             on_press=on_press,
             on_release=on_release) as listener:
@@ -203,13 +144,10 @@ def robot_drive_code():
     listener.start()
 
 
-
-
 if __name__ == "__main__":
-
 
     thread_robot_control = threading.Thread(target=robot_drive_code)
     thread_opencv = threading.Thread(target=opencv_code)
-  
+
     thread_opencv.start()
     thread_robot_control.start()
