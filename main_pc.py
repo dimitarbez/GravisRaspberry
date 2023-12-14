c277e95299680a6145c605d3498ac36d9704be7e
Dimitar Bezhanovski <30288047+dimitarbez@users.noreply.github.com>
2023-12-14 21:49:01 +0100

    updated the hud

--- main_pc.py
+++ main_pc.py
@@ -2,7 +2,7 @@ from time import sleep
 from pynput import keyboard
 import serial
 import threading
-from pyrplidar import PyRPlidar
+# from pyrplidar import PyRPlidar
 import math
 import numpy as np
 
@@ -10,18 +10,18 @@ import numpy as np
 import cv2 as cv
 
 # if using the picamera, import those libraries as well
-from picamera.array import PiRGBArray
-from picamera import PiCamera
+# from picamera.array import PiRGBArray
+# from picamera import PiCamera
 
 # point to the haar cascade file in the directory
 cascPath = "./haarcascades/haarcascade_frontalface_default.xml"
 faceCascade = cv.CascadeClassifier(cascPath)
 
 # #start the camera and define settings
-camera = PiCamera()
-camera.resolution = (640, 480)  # a smaller resolution means faster processing
-camera.framerate = 24
-rawCapture = PiRGBArray(camera, size=camera.resolution)
+# camera = PiCamera()
+# camera.resolution = (640, 480)  # a smaller resolution means faster processing
+# camera.framerate = 24
+# rawCapture = PiRGBArray(camera, size=camera.resolution)
 
 # set the distance between the edge of the screen
 # and the borders that trigger robot rotation
@@ -52,7 +52,7 @@ global camera_pitch
 camera_yaw = 90  # Initialize with the camera facing straight forward
 camera_pitch = 90  # Initialize with the camera level
 
-ser = serial.Serial('/dev/ttyUSB0', baudrate=115200, timeout=1)
+ser = serial.Serial('COM9', baudrate=115200, timeout=1)
 
 is_light_on = False
 
@@ -63,48 +63,39 @@ def on_press(key):
     global is_light_on
 
     try:
-        print('alphanumeric key {0} pressed'.format(
-            key.char))
-        if key.char == ('w'):
+        print('alphanumeric key {0} pressed'.format(key.char))
+
+        # Handling alphanumeric keys
+        if key.char == 'w':
             print('sent')
             ser.write('motor:forward\n'.encode())
-
-        if key.char == ('a'):
+        elif key.char == 'a':
             ser.write('motor:left\n'.encode())
-
-        if key.char == ('s'):
+        elif key.char == 's':
             print('sent')
             ser.write('motor:backward\n'.encode())
-
-        if key.char == ('d'):
+        elif key.char == 'd':
             print('sent')
             ser.write('motor:right\n'.encode())
-
-        elif key.char == ('1'):
+        elif key.char == '1':
             print('sent')
             ser.write('motor:speed:50\n'.encode())
-
-        elif key.char == ('2'):
+        elif key.char == '2':
             print('sent')
             ser.write('motor:speed:100\n'.encode())
-
-        elif key.char == ('3'):
+        elif key.char == '3':
             print('sent')
             ser.write('motor:speed:150\n'.encode())
-
-        elif key.char == ('4'):
+        elif key.char == '4':
             print('sent')
             ser.write('motor:speed:200\n'.encode())
-
-        elif key.char == ('5'):
+        elif key.char == '5':
             print('sent')
             ser.write('motor:speed:255\n'.encode())
-
-        elif key.char == ('p'):
+        elif key.char == 'p':
             print('sent')
             ser.write('servo:armheight:150\n'.encode())
-
-        elif key.char == ('o'):
+        elif key.char == 'o':
             print('sent')
             ser.write('servo:armheight:90\n'.encode())
         elif key.char == 'b':
@@ -142,16 +133,16 @@ def on_press(key):
             ser.write(f'servo:cam_pitch:{camera_pitch}\n'.encode())
 
 
-
-
 def on_release(key):
 
     ser.write('motor:stop\n'.encode())
 
     print('{0} released'.format(
         key))
+
     if key == keyboard.Key.esc:
         # Stop listener
+        quit()
         return False
 
 
@@ -176,6 +167,7 @@ def draw_camera_fov(frame, yaw, center, fov=60, radius=50, color=(0, 255, 0)):
 
 def read_serial_data():
     ser.flushInput()  # Clear the input buffer
+    sleep(0.1)
 
     # Request and read battery voltage
     ser.write('read:battery\n'.encode())
@@ -210,12 +202,14 @@ def opencv_code():
     global temperature
     global camera_yaw
 
-    # give camera time to warm up
+    cap = cv.VideoCapture(0)
     sleep(0.1)
-
-    for still in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True, resize=camera.resolution):
-        # take the frame as an array, convert it to black and white, and look for facial features
-        image = still.array
+    
+    counter = 0
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
 
         # Get frame dimensions
         height, width = frame.shape[:2]
@@ -235,18 +229,12 @@ def opencv_code():
         fov_center = (width - 100, height - 100)  # Position the FOV in the bottom right corner
         draw_camera_fov(frame, camera_yaw, fov_center)
 
-        # display the resulting image
-        cv.imshow("Display", image)
-
-        # clear the stream capture
-        rawCapture.truncate(0)
-
-        # set "q" as the key to exit the program when pressed
-        key = cv.waitKey(1) & 0xFF
-        if key == ord("q"):
+        cv.imshow('Display', frame)
+        if cv.waitKey(1) & 0xFF == ord('q'):
             break
 
     cap.release()
+    cv.destroyAllWindows()
 
 
 def robot_drive_code():
@@ -263,59 +251,12 @@ def robot_drive_code():
     listener.start()
 
 
-def lidar_code():
-
-    lidar = PyRPlidar()
-
-
-    sleep(2)
-
-    # Initialize the OpenCV window and frame
-    cv.namedWindow("RPLidar", cv.WINDOW_NORMAL)
-    cv.resizeWindow("RPLidar", 400, 400)
-    frame = 255 * np.zeros((400, 400, 3), dtype=np.uint8)
-
-
-    while True:
-        lidar.connect(port="/dev/ttyUSB1", baudrate=115200, timeout=3)
-        lidar.set_motor_pwm(500)
-
-        print(lidar.get_health())
-        print(lidar.get_info())
-        print(lidar.get_samplerate())
-
-        scan_generator = lidar.start_scan_express(4)
-        sleep(0.5)
-        for count, scan in enumerate(scan_generator()):
-            print(scan)
-            x = int(scan.distance * np.cos(np.radians(scan.angle)))
-            y = int(scan.distance * np.sin(np.radians(scan.angle)))
-            cv.circle(frame, (int(x/10) + 200, int(y/10) + 200),
-                    2, (0, 255, 0), -1)
-
-            # Display the frame
-            if count % 10 == 0:
-                cv.imshow("RPLidar", frame)
-                cv.waitKey(1)
-
-
-            if count > 4000:
-                frame = 255 * np.zeros((400, 400, 3), dtype=np.uint8)
-                break
-
-        lidar.stop()
-        lidar.disconnect()
-        sleep(2)
-
-    #cv.destroyAllWindows()
 
 
 if __name__ == "__main__":
 
     thread_robot_control = threading.Thread(target=robot_drive_code)
     thread_opencv = threading.Thread(target=opencv_code)
-    thread_lidar = threading.Thread(target=lidar_code)
 
-    thread_lidar.start()
     thread_opencv.start()
     thread_robot_control.start()
